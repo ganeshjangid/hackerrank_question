e878acc6923eb1b9f88c7417de9178a4d76400bb
ganeshjangid <webdeveloper.ganesh@gmail.com>
2019-09-30 13:59:58 +0530

    test

--- test4.js
+++ test4.js
@@ -1,41 +1,39 @@
 // Complete the sockMerchant function below.
 function sockMerchant(n, ar) {
-    //console.log(ar);
+    //console.log(n);
+     
+    let matchCheck=[];
+    let pairInsert=[];
 
-    let match=[];
-    let get=[];
-    let result=[];
-    //[10,20,20,10,10,30,50,10,20];
-    ar.forEach((v,i) => {
-        //console.log(v);
-       // if (match.indexOf(v) != -1) {
-        if (match.indexOf(v) != -1) {
-            //console.log("yes");
-            get.push(v);
+
+    ar.forEach(val => {
+        //console.log(val);
+        
+        if (matchCheck.indexOf(val) != -1) {
+            //console.log("exist");
+
+            if (!pairInsert.includes(val)) {
+                pairInsert.push(val);
+            }
+
+
         }else{
-            //console.log("no");
-            match.push(v);
+            //console.log("not exist");
+            matchCheck.push(val);
         }
 
-       // console.log(get);
-        
-       if (get[v]) {
-           result.push(v)
-       }
-
-        /* console.log(match);
-        console.log(get); */
     });
-    
-   console.log(get);
-    //console.log(result);
 
-    return result.length;
+    //console.log(matchCheck);
     
+    //console.log(pairInsert);
 
+    return pairInsert.length;
+    
 }
 //let ar = [1,2,1,2,1,3,2];
-let ar = [10,20,20,10,10,30,50,10,20];
+//let ar = [10,20,20,10,10,30,50,10,20]; //[20,10,10,10,20] [20,10,]
+let ar = [1,1,3,1,2,1,3,3,3,3]; //[20,10,10,10,20] [20,10,]
 
 let n=7;
 
